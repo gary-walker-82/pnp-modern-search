accc28bbc00328a725cfc1c29d24e8bb51dc5857
Gary Walker <gary@claroleague.co.uk>
2021-09-09 16:19:26 +0100

    added ability to add more layouts

--- search-extensibility/src/models/IExtensibilityLibrary.ts
+++ search-extensibility/src/models/IExtensibilityLibrary.ts
@@ -1,22 +1,27 @@
-import { IComponentDefinition } from './layouts/IComponentDefinition';
-import { ISuggestionProviderDefinition } from './suggestions/ISuggestionProviderDefinition';
-import * as Handlebars from 'handlebars';
+import { IComponentDefinition } from "./layouts/IComponentDefinition";
+import { ISuggestionProviderDefinition } from "./suggestions/ISuggestionProviderDefinition";
+import * as Handlebars from "handlebars";
+import { ILayoutDefinition } from "./layouts/ILayoutDefinition";
 
 export interface IExtensibilityLibrary {
-    
-    /**
-     * Returns custom web components
-     */
-    getCustomWebComponents(): IComponentDefinition<any>[];
+  /**
+   * Returns custom web components
+   */
+  getCustomWebComponents(): IComponentDefinition<any>[];
+  /**
+   * Returns custom layouts
+   */
+  getCustomLayouts(): ILayoutDefinition[];
+  /**
+   * Returns custom suggestions providers
+   */
+  getCustomSuggestionProviders(): ISuggestionProviderDefinition[];
 
-    /**
-     * Returns custom suggestions providers
-     */
-    getCustomSuggestionProviders(): ISuggestionProviderDefinition[];
-
-    /**
-     * Allows to register Handlebars customizations like helpers or partials in the current Web Part Handlebars isolated namespace
-     * @param namespace the current Web Part Handlebars namespace
-     */
-    registerHandlebarsCustomizations?(handlebarsNamespace: typeof Handlebars): void;
+  /**
+   * Allows to register Handlebars customizations like helpers or partials in the current Web Part Handlebars isolated namespace
+   * @param namespace the current Web Part Handlebars namespace
+   */
+  registerHandlebarsCustomizations?(
+    handlebarsNamespace: typeof Handlebars
+  ): void;
 }
